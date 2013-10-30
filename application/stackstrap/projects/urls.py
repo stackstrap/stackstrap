4d8d30560ea40ccccd83205be8957a77a86205c9
Evan Borgstrom <evan@fatbox.ca>
2013-10-30 00:07:11 -0400

    Lots more work and refactoring.
    Need to switch from the git python module to using the sh module. I'm
    finding the git module to be a PITA to work with. See the comments in
    the SO answer about using the sh module:
    http://stackoverflow.com/a/8578096

--- application/stackstrap/projects/urls.py
+++ application/stackstrap/projects/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 
+from .views import ProjectsView, ZipDownload
+
 urlpatterns = patterns(
         '',
 
-        url(r'(?P<project_id>\d+)/zip', 'projects.views.zip')
+        url(r'(?P<project_id>\d+)/zip', ZipDownload.as_view(), name='project_zip'),
+        url(r'', ProjectsView.as_view(), name='projects')
         )
