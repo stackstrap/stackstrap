92a41ec5e60309001d3e0a2697b61ef3f4648053
Evan Borgstrom <evan@fatbox.ca>
2013-10-30 11:25:57 -0400

    Refactor & implement templating with git repo

--- README.rst
+++ README.rst
@@ -22,6 +22,38 @@ easy to use web interface, built using Django, that allows you to quickly and
 reliably create development environments (and in the future provision your
 production servers too).
 
+Template Metadata Format
+------------------------
+Inside of template repositories you can create a special file name named
+`stackstrap` that contains metadata informing the template system of which
+files need to be parsed as templates and which directories need to be renamed.
+
+.. code-block:: yaml
+    ---
+    stackstrap:
+      path_templates:
+        'project_app/something/else': 'project_app/something/{{ project.id }}'
+        'project_app': '{{ project.name }}_app'
+
+      file_templates:
+        - deployment/deploy.rb
+
+The `path_templates` entries define paths that should be transformed based on
+the current context. They are evaluated in the order that they are defined and
+are relative to the root of the template. The example above shows how you would
+process a nested directory first and then its parent directory second.
+
+The `file_templates` entries define files, again relative to the root of the
+template, which should be parsed using `Django's template system` before they
+are sent to the user.
+
+When both path & file templates are parsed they are provided a context that
+contains the following items to use:
+
+* `project` -- the current project object
+* `membership` -- the membership to the project, from here you can get the
+                  current user and any related attributes
+
 Development of StackStrap
 -------------------------
 
@@ -33,6 +65,7 @@ You need to have the following software installed on your machine:
 If this is the first time this vagrant instance will be brought up then you
 need to generate your master & minion keys so thatt hey can be preseeded:
 
+.. code-block::
     openssl genrsa -out salt/keys/master.pem 2048
     openssl rsa -in salt/keys/master.pem -pubout > salt/keys/master.pub
     openssl genrsa -out salt/keys/minion.pem 2048
@@ -47,3 +80,7 @@ development instance you setup.
 .. _Salt: http://saltstack.com/
 .. _provisioners: http://docs.vagrantup.com/v2/provisioning/index.html
 .. _community repository: https://github.com/fatbox/stackstrap-salt
+.. _Django's template system: https://docs.djangoproject.com/en/dev/ref/templates/
+
+
+.. vim: set ts=4 sw=4 sts=4 et ai :
--- application/stackstrap/projects/views.py
+++ application/stackstrap/projects/views.py
@@ -1,13 +1,6 @@
-import os
-import shutil
-import StringIO
-import tempfile
-import zipfile
-
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from django.template.loader import render_to_string
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.views.generic import ListView
@@ -39,57 +32,7 @@ class ZipDownload(View):
         project = get_object_or_404(Project, id=project_id)
         membership = get_object_or_404(Membership, project=project, user=request.user)
 
-        # get our temp dir
-        temp_dir = tempfile.mkdtemp()
-        temp_dir_len = len(temp_dir) + 1
-
-        def temp_file(*args):
-            "closure to make generating temp file names easier"
-            return os.path.join(temp_dir, *args)
-
-        # make the rest of our dirs
-        os.mkdir(temp_file('salt'))
-        os.mkdir(temp_file('salt', 'keys'))
-
-        # build our context
-        context = {
-                'user': request.user,
-                'project': project,
-                'membership': membership,
-                }
-
-        # render our files
-        with open(temp_file('Vagrantfile'), 'w') as f:
-            f.write(render_to_string('projects/Vagrantfile', context))
-
-        with open(temp_file('salt', 'minion'), 'w') as f:
-            f.write(render_to_string('projects/salt.minion', context))
-
-        with open(temp_file('salt', 'keys', 'minion.pem'), 'w') as f:
-            f.write(membership.private_key.read())
-
-        with open(temp_file('salt', 'keys', 'minion.pub'), 'w') as f:
-            f.write(membership.public_key.read())
-
-        # build our zip file to be returned to the user
-        zip_io = StringIO.StringIO()
-        zip_file = zipfile.ZipFile(zip_io, "w")
-
-        # recursively add our files
-        for base, dirs, files in os.walk(temp_dir):
-            for f in files:
-                # build the full name
-                zip_name = os.path.join(base, f)
-
-                # write the file relative to the top of the temp dir
-                zip_file.write(zip_name, zip_name[temp_dir_len:])
-
-        zip_file.close()
-
-        # clean up our tempdir
-        shutil.rmtree(temp_dir)
-
         # send our response
-        resp = HttpResponse(zip_io.getvalue(), mimetype = "application/x-zip-compressed")
-        resp['Content-Disposition'] = 'attachment; filename=StackStrap-Project-Template_%d-%s.zip' % (project.id, project.name)
+        resp = HttpResponse(project.make_zip(membership), mimetype = "application/x-zip-compressed")
+        resp['Content-Disposition'] = 'attachment; filename=StackStrap_%d-%s.zip' % (project.id, project.name)
         return resp
