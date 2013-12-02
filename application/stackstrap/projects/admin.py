54fe09c30f5cf72bbcb476d251b9ada26fb0550b
Howard Roark <howie@codingcoop.org>
2013-12-02 15:15:18 -0500

    removed "members" from projects app django admin file

--- application/stackstrap/projects/admin.py
+++ application/stackstrap/projects/admin.py
@@ -24,14 +24,10 @@ class BoxAdmin(admin.ModelAdmin):
     list_display = ('name', 'url')
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'short_name', '_members', 'box', 'template')
+    list_display = ('name', 'short_name', 'box', 'template')
     list_filter = ('box', 'template')
     search_fields = ('name',)
     readonly_fields = ('public_key', 'private_key')
-
-    def _members(self, obj):
-        return ", ".join([str(m) for m in obj.members.all()])
-    _members.short_description = "Members"
 
 admin.site.register(Template, TemplateAdmin)
 admin.site.register(Box, BoxAdmin)
