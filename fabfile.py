1f07912a9aa86e9f754636da8adf65309e86ca80
Evan Borgstrom <evan@fatbox.ca>
2013-12-26 22:32:12 -0500

    Use the new Vagrant wrapper from Garment to run django-admin commands

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,12 @@
 from garment import *
+from garment.vagrant import vagrant
+
+import fabric.api as fab
+
+@fab.task
+def django_admin(command):
+    vagrant.sudo('/bin/sh -c ". /home/stackstrap/virtualenv/bin/activate; PYTHONPATH=/home/stackstrap/current/application/stackstrap DJANGO_SETTINGS_MODULE=stackstrap.settings.dev django-admin.py %s"' % command, user='stackstrap')
+
+@fab.task
+def runserver():
+    django_admin("runserver 0:6000")
