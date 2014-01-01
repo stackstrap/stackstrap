c521fa6f0bd6bffdf5feda47714f38d0877c80fc
Evan Borgstrom <evan@fatbox.ca>
2013-12-31 23:48:40 -0500

    Overhaul the salt integration to use our new strategy

--- stackstrap/commands/create.py
+++ stackstrap/commands/create.py
@@ -1,4 +1,6 @@
+import inspect
 import os
+import shutil
 import yaml
 
 from . import Command
@@ -35,12 +37,26 @@ class Create(Command):
             default='http://files.vagrantup.com/precise32.box'
         )
         self.parser.add_argument(
+            '-B', '--box-name',
+            dest='box_name',
+            metavar='NAME',
+            type=str,
+            help='The name of the box, defaults to being automatically derived from the URL'
+        )
+        self.parser.add_argument(
             '-r', '--ref',
             dest='ref',
             metavar='REF',
             type=str,
             help='The GIT ref of the template to use when creating the project, defaults to master',
             default='master'
+        )
+        self.parser.add_argument(
+            '-P', '--nopull',
+            dest='nopull',
+            action='store_true',
+            help='Skip pulling from origin prior to creation',
+            default=False
         )
 
     def main(self, args):
@@ -56,8 +72,13 @@ class Create(Command):
         ))
 
         # access the repository and archive it to our destination project name
-        template_repo = Repository(args.template)
+        template_repo = Repository(args.template, nopull=args.nopull)
         template_repo.archive_to(args.ref, args.name)
+
+        if 'box_name' in args and args.box_name:
+            box_name = args.box_name
+        else:
+            box_name = os.path.basename(args.box).strip(".box")
 
         # for backwards compatibility we provide a mock project object
         class Project(object):
@@ -67,7 +88,7 @@ class Create(Command):
         template_context = {
             'name': args.name,
             'box_url': args.box,
-            'box_name': os.path.basename(args.box).strip(".box"),
+            'box_name': box_name,
 
             # see above
             'project': Project()
@@ -85,37 +106,75 @@ class Create(Command):
         def path(*parts):
             return os.path.join(args.name, *parts)
 
-        def render(dest, source):
-            with open(dest, 'w') as f:
-                f.write(template.render(source))
-
-        def render_in_place(source):
-            data = open(source).read()
+        def render_in_place(*parts):
+            source = path(*parts)
+            data = unicode(open(source).read(), "utf8")
             with open(source, 'w') as f:
                 f.write(template.render_string(data))
 
-        mkdir('salt')
-        mkdir('salt', 'root')
+        # recurse over our template and copy it in into place
+        self.log.debug("Copying project template into place...")
 
-        render(path('Vagrantfile'), 'Vagrantfile')
-        render(path('salt', 'minion'), 'salt.minion')
+        base_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        template_dir = os.path.abspath(os.path.join(base_dir, "..", "project_template"))
+        self.log.debug("Processing: {0}".format(template_dir))
 
-        # read the meta-data
+        for root, folders, files in os.walk(template_dir):
+            relative_dir = root.replace(template_dir, '.')
+            self.log.debug(relative_dir)
+
+            for f in files:
+                self.log.debug(os.path.join(relative_dir, f))
+
+                shutil.copyfile(os.path.join(root, f), path(relative_dir, f))
+                try:
+                    render_in_place(relative_dir, f)
+                except UnicodeDecodeError:
+                    self.log.warn("Failed to render template due to unicode errors: {0}".format(
+                        path(relative_dir, f)
+                    ))
+
+            for f in folders:
+                mkdir(relative_dir, f)
+
+        # read the metadata
         # it must be processed as a template, then loaded as YAML
+        self.log.debug("Loading template metadata...")
         metadata = yaml.load(template.render('stackstrap/meta.yml'))
 
+        # move the salt files into place
+        if os.path.exists(path('stackstrap', 'state.sls')):
+            self.log.debug("Copying template state file to salt/root/%s.sls" % args.name)
+            shutil.copyfile(
+                path('stackstrap', 'state.sls'),
+                path('salt', 'root', '%s.sls' % args.name)
+            )
+        if os.path.exists(path('stackstrap', 'pillar.sls')):
+            self.log.debug("Copying template pillar file to salt/pillar/%s.sls" % args.name)
+            shutil.copyfile(
+                path('stackstrap', 'pillar.sls'),
+                path('salt', 'pillar', '%s.sls' % args.name)
+            )
+
         # iterate the files to parse with Django templates
+        self.log.debug("Processing file templates...")
         file_template_paths = metadata.get("file_templates", [])
         file_template_paths.append("stackstrap/meta.yml")
         for p in file_template_paths:
-            render_in_place(path(p))
+            self.log.debug(p)
+            render_in_place(p)
 
         # iterate the paths to update with custom names
+        self.log.debug("Processing path templates...")
         path_templates = metadata.get("path_templates", [])
         for path_template in path_templates:
             for orig_path in path_template:
+                self.log.debug("%s -> %s" % (orig_path, path_template[orig_path]))
                 os.rename(path(orig_path),
                           path(path_template[orig_path]))
+
+        # now destroy the stackstrap directory as it's no longer needed
+        shutil.rmtree(path('stackstrap'), ignore_errors=True)
 
         self.log.info("Done!\nYou can now 'vagrant up' your development environment from the {name} folder".format(
             name=args.name
---
+++ stackstrap/project_template/salt/root/top.sls
@@ -0,0 +1,13 @@
+#
+# StackStrap auto-generated state top.sls file
+#
+# Generated using the following template:
+# {{ name }} - {{ template_url }}
+#
+
+base:
+  '*':
+    - stackstrap
+    - {{ name }}
+
+# vim: set ft=yaml et sw=2 ts=2 sts=2 :
