edb539d0d9ca22122e8808d7c6f6ce277809cb0e
Evan Borgstrom <evan@fatbox.ca>
2014-01-03 23:04:07 -0500

    Tons of refactoring as I continue to implement the template interface
    * This adds a Settings class that will be used to store user
      configurable settings, for the time being it is used to configure the
      directory to be used as the base for config & cache files.
    
    * Total overhaul on the test suite to utilize unittest now.
    
    * Lots of general improvements

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,22 +1,49 @@
+import logging
 import os
 import shutil
+import tempfile
+import unittest
+
+from stackstrap.config import settings
 
 base_dir = os.path.dirname(__file__)
 test_repos = (
     'test_repo',
-    'test_template'
+    'test_template',
+    'test_template_bad',
 )
 
-def setup_repositories():
-    """
-    Since we can't distribute a git respository in a git repository we use a
-    hack and keep the .git dir as _git. This renames them back to .git
-    """
-    for repo in test_repos:
-        shutil.move(os.path.join(base_dir, repo, '_git'),
-                    os.path.join(base_dir, repo, '.git'))
+class StackStrapTestCase(unittest.TestCase):
+    def setUp(self):
+        self.log = logging.getLogger("test:%s" % self.__class__.__name__)
 
-def restore_repositories():
-    for repo in test_repos:
-        shutil.move(os.path.join(base_dir, repo, '.git'),
-                    os.path.join(base_dir, repo, '_git'))
+        # reconfigure our settings to use a temp directory
+        tmp_dir = tempfile.mkdtemp()
+        settings.configure(tmp_dir)
+        self.log.debug("Configured settings to use: %s" % tmp_dir)
+
+        self.git_fixup('_git', '.git')
+
+    def tearDown(self):
+        # restore the .git dirs back to _git
+        self.git_fixup('.git', '_git')
+
+        # remove the settings directory we setup
+        try:
+            shutil.rmtree(settings.base_dir)
+        except OSError:
+            if os.path.isdir(settings.base_dir):
+                raise
+
+    def git_fixup(self, orig, dest):
+        # Since we can't distribute a git respository in a git repository we use a
+        # hack and keep the .git dir as _git. This renames them back to .git
+        for repo in test_repos:
+            orig_path = os.path.join(base_dir, repo, orig)
+            dest_path = os.path.join(base_dir, repo, dest)
+            self.log.debug("Renaming %s -> %s" % (orig_path, dest_path))
+            try:
+                shutil.move(orig_path, dest_path)
+            except IOError:
+                if not os.path.isdir(dest_path):
+                    raise
--- tests/test_template.py
+++ tests/test_template.py
@@ -2,38 +2,60 @@ import os
 import shutil
 import tempfile
 
-from nose import with_setup
+from stackstrap.template import Template, TemplateRepoException, TemplateMetaException
 
-from stackstrap.template import Template
+from . import StackStrapTestCase
 
-from . import setup_repositories, restore_repositories
 
 # setup our repo & cache urls & dirs
 repo_url = 'file://{0}/test_template/'.format(os.path.dirname(__file__))
+bad_repo_url = 'file://{0}/test_template_bad/'.format(os.path.dirname(__file__))
+missing_repo_url = 'file://{0}/test_template_missing/'.format(os.path.dirname(__file__))
 
-@with_setup(setup_repositories, restore_repositories)
-def test_template_load_save():
-    # put our templates into a temporary dir that we can cleanup
-    tmp_dir = tempfile.mkdtemp()
-    Template.template_dir = tmp_dir
 
-    try:
-        template = Template(
-            'test-template',
-            repo_url,
-            'master',
-            'http://files.vagrantup.com/precise32.box'
-        )
+def make_template(template_url=repo_url):
+    return Template(
+        'test-template',
+        template_url,
+        'master',
+        'http://files.vagrantup.com/precise32.box'
+    )
+
+
+class TemplateTestCase(StackStrapTestCase):
+    def test_template_load_save(self):
+        template = make_template()
 
         # save the file
-        assert not os.path.exists(os.path.join(tmp_dir, 'test-template'))
+        self.assertFalse(os.path.exists(template.template_file))
         template.save()
-        assert os.path.exists(os.path.join(tmp_dir, 'test-template'))
+        self.assertTrue(os.path.exists(template.template_file))
+
+        # make sure the file is what we're expecting
+        expected = "{box: 'http://files.vagrantup.com/precise32.box', box_name: precise32, name: test-template,\n  nopull: false, ref: master, url: 'file:///Users/evan/Projects/stackstrap/tests/test_template/'}\n"
+        self.assertEqual(expected, open(template.template_file).read())
 
         # re-load it
         template.ref = None
-        assert template.ref is None
+        self.assertIsNone(template.ref)
         template = Template.load('test-template')
-        assert template.ref == 'master'
-    finally:
-        shutil.rmtree(tmp_dir)
+        self.assertEqual(template.ref, 'master')
+
+
+    def test_template_validate(self):
+        template = make_template()
+        template.validate()
+        assert template.meta['template_name'] == 'Test Template'
+        assert template.meta['template_description'] == 'Testing'
+        assert template.meta['file_templates'] == ['README']
+        assert template.meta['path_templates'] == [{'README': 'README.transformed'}]
+
+
+    def test_template_validate_fail_bad_meta(self):
+        template = make_template(bad_repo_url)
+        self.assertRaises(TemplateMetaException, template.validate)
+
+
+    def test_template_validate_fail_bad_repo(self):
+        template = make_template(missing_repo_url)
+        self.assertRaises(TemplateRepoException, template.validate)
